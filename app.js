75bed27d859a3e1c6bad5662e042aeb6b394ee20
jacobrigal <webdevjacobrigal@gmail.com>
2020-06-11 00:27:27 -0500

    Added a 24 hour clock and an onload prompt to enter a name, which displays in h1.

--- app.js
+++ app.js
@@ -34,3 +34,30 @@ function myFunction() {
     }
     document.getElementById("confirm").innerHTML = txt;
   }
+
+  function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('txt').innerHTML =
+    h + ":" + m + ":" + s;
+    var t = setTimeout(startTime, 500);
+  }
+  function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+  }
+
+  function yourName() {
+    var txt;
+    var person = prompt("Please enter your name:", "(Name)");
+    if (person == null || person == "") {
+      txt = " with no-name";
+    } else {
+      txt = " for " + person;
+    }
+    document.getElementById("name").innerHTML = txt;
+  }
